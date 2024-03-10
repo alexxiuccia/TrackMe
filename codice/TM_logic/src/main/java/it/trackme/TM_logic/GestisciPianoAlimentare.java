c90af78b34bc2fdfc77b1cf57493ce011b8a27d1
Nicole772 <145766507+Nicole772@users.noreply.github.com>
2024-03-10 14:10:44 +0100

    update
    metodo per il calcolo delle calorie medie in un determinato periodo

--- codice/TM_logic/src/main/java/it/trackme/TM_logic/GestisciPianoAlimentare.java
+++ codice/TM_logic/src/main/java/it/trackme/TM_logic/GestisciPianoAlimentare.java
@@ -48,5 +48,36 @@ public class GestisciPianoAlimentare {
 
 		System.out.println("Calorie totali consumate oggi: " + totalCalories);
 	}
+	public static void consumoPeriodo(LocalDate inizio, LocalDate fine)
+	{
+		DSLContext create = DSL.using(DBconnection.getConnection(), SQLDialect.SQLITE);
+		int totalCalories = 0;
+		int cont=0;
+	
+		for (LocalDate dataCorrente = inizio; !dataCorrente.isAfter(fine); dataCorrente = dataCorrente.plusDays(1))
+		{
+			cont++;
+			Result<Record1<Integer>> result = create
+				    	.select(DSL.sum(Alimento.ALIMENTO.CALORIE
+				                    	.mul(Composizionericetta.COMPOSIZIONERICETTA.DOSE)
+				                    	.mul(Creazionepasto.CREAZIONEPASTO.QUANTITÀ))
+				                    	.cast(Integer.class))
+				    	.from(Pasto.PASTO)
+				    	.join(Creazionepasto.CREAZIONEPASTO)
+				    	.on(Pasto.PASTO.IDPASTO.eq(Creazionepasto.CREAZIONEPASTO.IDPASTO))
+				    	.join(Composizionericetta.COMPOSIZIONERICETTA)
+				    	.on(Creazionepasto.CREAZIONEPASTO.IDRICETTA.eq(Composizionericetta.COMPOSIZIONERICETTA.IDRICETTA))
+				    	.join(Alimento.ALIMENTO)
+				    	.on(Composizionericetta.COMPOSIZIONERICETTA.IDALIMENTO.eq(Alimento.ALIMENTO.IDALIMENTO))
+				    	.where(Pasto.PASTO.DATA.eq(dataCorrente))
+				    	.groupBy(Pasto.PASTO.IDPASTO)
+				    	.fetch();
 
+				
+					for (Record1<Integer> record : result) {
+						totalCalories += record.value1();
+					}
+		}
+	  System.out.println("Le calorie medie assunte giornalmente dal "+ inizio+" al "+fine+ " sono "+totalCalories/cont);
+	}
 }

3774ce97ff1ec1aea9e1cd364ce4e5669493f082
Nicole772 <145766507+Nicole772@users.noreply.github.com>
2024-03-15 12:10:58 +0100

    update

--- codice/TM_logic/src/test/java/it/trackme/TM_logic/GestisciPianoAlimentareTest.java
+++ codice/TM_logic/src/test/java/it/trackme/TM_logic/GestisciPianoAlimentareTest.java
@@ -10,22 +10,7 @@ public class GestisciPianoAlimentareTest {
 
 	@Test
 	public void test() {
-		int idAlimento=0;
-		
-		idAlimento=GestisciAlimento.inserisciNuovoAlimento("Pollo", 76, 20, 3.6f);
-		int idRicetta=GestisciRicetta.nuovaRicetta("Pollo con riso e zucchine", 100 );
-		GestisciComposizioneRicetta.ComposizioneRicetta(idRicetta, idAlimento, 80);
-		
-		idAlimento=GestisciAlimento.inserisciNuovoAlimento("Zucchine", 3.11f, 1.21f, 0.32f);
-		GestisciComposizioneRicetta.ComposizioneRicetta(idRicetta, idAlimento, 20);
-		
-		idAlimento=GestisciAlimento.inserisciNuovoAlimento("Riso", 80.4f, 6.7f, 0.4f);
-		GestisciComposizioneRicetta.ComposizioneRicetta(idRicetta, idAlimento, 60);
-		
 		int idUtente=1;
-		LocalDate oggi = LocalDate.now();
-		int idPasto= GestisciPasto.inserisciPasto(idUtente, oggi, "pranzo");
-		GestisciPasto.inserisciDoseRicettaInPasto(idPasto, idRicetta, 2);
 		
 		GestisciPianoAlimentare.consumoDelGiorno(idUtente);
 		fail("Test non riuscito");
